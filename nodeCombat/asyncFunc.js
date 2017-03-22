d35489471f0d7ecaa8b2b4ceae1b76c37ef23bf8
roy-lau <roy-lau@foxmail.com>
2017-03-22 15:39:19 +0800

    uncode

---
+++ nodeCombat/asyncFunc.js
@@ -0,0 +1,10 @@
+function asyncFunc(cb){
+   setTimeout(cb,200);      // 异步(blue)
+//  cb();                   // 同步(green)
+}
+var color = 'blue';
+asyncFunc(function(){
+  console.log('This color is:' + color);
+});
+
+color = 'green';
