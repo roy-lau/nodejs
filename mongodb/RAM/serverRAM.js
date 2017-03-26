227a0c40dda2870aa0fbcc166320ebaace1fa252
roy-lau <roy-lau@foxmail.com>
2017-03-26 12:17:09 +0800

    add RAM cli

--- mongodb/RAM/serverRAM.js
+++ mongodb/RAM/serverRAM.js
@@ -1,14 +1,15 @@
-﻿var http = require('http');
-
+// 将访问次数放到内存上
+var http = require('http');
+var counter = 0;
 http.createServer(function (request, response) {
-  
+  counter++;
 	// 发送 HTTP 头部 
 	// HTTP 状态值: 200 : OK
 	// 内容类型: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.writeHead(200, {'Content-Type': 'text/plain'},'utf8');
 
 	// 发送响应数据 "Hello World"
-	response.end('Hello World\n');
+	response.end('您是第： ' + counter + '  次访问');
 }).listen(8888);
 
 // 终端打印如下信息
