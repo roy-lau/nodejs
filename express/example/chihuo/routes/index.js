3794a194fb262344880c9d93a635eec9ebfc5422
roy-lau <roy-lau@foxmail.com>
2017-03-29 14:09:34 +0800

    take chihuo modify :wrench: is photo add :zap:

--- express/example/chihuo/routes/index.js
+++ express/example/chihuo/routes/index.js
@@ -14,19 +14,19 @@ router.get('/login', function(req, res) {
 		})
 
 /* ucenter */
-router.post('/ucenter', function(req,res){
-		var query = {name: req.body.name, password:req.body.password};
+router.post('/ucenter', function(req, res){
+		var query = {name: req.body.name, password: req.body.password};
 		(function(){
-		 user.cont(query, function(err, doc){	// cont返回集合中文档的数量，和find一样可以接收查询条件。query表示查询的条件
+		 user.count(query, function(err, doc){	// cont返回集合中文档的数量，和find一样可以接收查询条件。query表示查询的条件
 				 if(doc == 1){
-				 console.log(query.name + ":登陆成功" + new Date());
-				 res.render('ucenter', {title: 'ucenter'});
+					 console.log(query.name + ":登陆成功 || " + new Date());
+					 res.render('ucenter', { title: doc});
 				 }else{
-				 console.log(query.name + ":登陆失败" + new Date());
-				 res.redirect('/');
+					 console.log(query.name + ":登陆失败" + new Date());
+					 res.redirect('/');
 				 }
-			})
+			});
 		 })(query);
-		});
+	});
 
 module.exports = router;
--- express/example/photo/app.js
+++ express/example/photo/app.js
@@ -7,28 +7,25 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var photos = require('./routes/photos');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-app.set('view engine', 'html');
-app.engine('.html',require('ejs').__express); 	// 两个下划线
-
-
-
+app.set('views', path.join(__dirname, 'views'));    // 配置视图文件
+app.set('view engine', 'ejs');      				// 配置视图模板
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));         // 输出有颜色区分的日志，便于开发调试
+app.use(bodyParser.json());     // 解析请求体
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));    // 提供./pulic下的静态文件
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/photos', photos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,10 +40,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // 返回错误页
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(8888);
+
 module.exports = app;
-console.log("服务器启动：localhost:8888")
