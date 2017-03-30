6154f7d64006bfc066f6c7114ad4cb4a8956cf9d
roy-lau <roy-lau@foxmail.com>
2017-03-30 15:34:46 +0800

    :zap: 吼吼箱

--- express/example/photo/routes/upload.js
+++ express/example/photo/routes/upload.js
@@ -16,15 +16,17 @@ module.exports = {
 			 *
 			 * 1.获取上传文件的文件名(已解决)
 			 * 2.获取上传文件的全路径
+			 *  	img.name 应该获取上传文件的文件名
+			 *		img.path 应该获取文件的路径
 			 */
 			 console.log(req.body)
 			 console.log(req.files)
-			var  img = req.body.photosImage		// 这里应该获取本地文件的全部路径
+			var  img = req.body.photosImage		
 				name = req.body.photosName,
-				path = join(dir, img );
+				path = join(dir, img.name );
 
 			// 文件重命名	
-			fs.rename( img , path, function(err){
+			fs.rename( img.path , path, function(err){
 				if (err)  return next(err);
 
 				db.create({
