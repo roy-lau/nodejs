4ca8163d372f197489c1108f8ad002ded29c178a
roy-lau <roy-lau@foxmail.com>
2017-05-17 10:38:49 +0800

    :one::bug: 缩进问题，:two::bug:后台数据typeErr,:three::bug:路由问题，:four::bug: 导航列表

--- express/project/movieWebsite-1.0/routes/index.js
+++ express/project/movieWebsite-1.0/routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-
     /* GET home page. */
     router.get('/', function(req, res, next) {
         res.render('pages/index', {
@@ -10,31 +8,31 @@ var router = express.Router();
             movies: [{
                 title: "1-1.项目前期准备",
                 _id: 1,
-                poster: "../video/movie-1.1.mp4"
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg"
             }, {
                 title: "2-1.nodejs入口文件分析和初始化",
                 _id: 2,
-                poster: "../video/movie-1.2.mp4"
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg"
             }, {
                 title: "2-2.创建jade视图",
                 _id: 3,
-                poster: "../video/movie-1.3.mp4"
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg"
             }, {
                 title: "2-3.伪造模板，跑通前后端",
                 _id: 4,
-                poster: "../video/movie-1.4.mp4"
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg"
             }, {
                 title: "3-1.mongodb模型设计及编码",
                 _id: 5,
-                poster: "../video/movie-1.5.mp4"
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg"
             }, {
                 title: "3-2.编写数据库交互代码",
                 _id: 6,
-                poster: "../video/movie-1.6.mp4"
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg"
             }, {
                 title: "4-1.删除功能及项目生成文件",
                 _id: 7,
-                poster: "../video/movie-1.7.mp4"
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg"
             }]
         });
     });
@@ -43,14 +41,14 @@ var router = express.Router();
     router.get('/movie/:id', function(req, res, next) {
         res.render('pages/detail', {
             title: '电影网站——详情页',
-            movie: [{
+            movies: [{
                 doctor: "Scott",
                 country: "中国",
                 title: "nodejs 100分钟建站攻略！",
                 year: 2017,
-                poster: "../video/movie-1.7",
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg",
                 language: "中文",
-                flash: "../video/movie-1.7",
+                flash: "http://js.tudouui.com/bin/lingtong/PortalPlayer_199.swf",
                 summary: "想了解前后端同力合作的整个作业流程吗？本课程带你实现一个从前端到后端的项目，包括nodejs，express，mongodb，jade模板引擎的使用，以及bootstrap/jQuery的实际应用及场景评估，让你更好的窥探前端的职业发展，为进一步快速自学打下基础。"
             }]
         });
@@ -77,15 +75,15 @@ var router = express.Router();
     router.get('/admin/list', function(req, res, next) {
         res.render('pages/list', {
             title: '电影网站——列表页',
-            movie: [{
+            movies: [{
                 title: "nodejs 100分钟",
                 _id: 1,
                 doctor: "Scott",
                 country: "中国",
                 year: 2015,
-                poster: "../video/movie-1.7.mp4",
+                poster: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1494987250&di=af3b9ed7539650f528d06cf1043ef093&src=http://i-7.vcimg.com/trim/2a2eb373b42812501d52bcf9c72d670f53461/trim.jpg",
                 language: "中文",
-                flash: "../video/movie-1.7.mp4",
+                flash: "http://js.tudouui.com/bin/lingtong/PortalPlayer_199.swf",
                 summary: "想了解前后端同力合作的整个作业流程吗？本课程带你实现一个从前端到后端的项目，包括nodejs，express，mongodb，jade模板引擎的使用，以及bootstrap/jQuery的实际应用及场景评估，让你更好的窥探前端的职业发展，为进一步快速自学打下基础。"
             }]
         });
