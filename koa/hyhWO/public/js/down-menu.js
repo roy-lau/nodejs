99275344da9268b206f48b33385856c1a8c9126f
roy-lau <roy-lau@foxmail.com>
2017-06-16 10:34:17 +0800

    koa hyhWO subMenu add

--- koa/hyhWO/public/js/down-menu.js
+++ koa/hyhWO/public/js/down-menu.js
@@ -1,9 +1,11 @@
 	// 控制显示、隐藏子菜单
 	//- 产品
-	$('#product_item').hover(function() {
-	    $('#product_menu').stop(true, true);
-	    $('#product_menu').slideDown();
-	}, function() {
-	    // $('#product_menu').stop(true, true);
-	    $('#product_menu').slideUp();
-	});
+/*	$(function(){
+		$('#product_item').hover(function() {
+		    $('#product_menu').stop(true, true);
+		    $('#product_menu').slideDown();
+		}, function() {
+		    // $('#product_menu').stop(true, true);
+		    $('#product_menu').slideUp();
+		});
+	})*/
