a3c4f20172c921d77668476b1e0371b7d63acc62
roy-lau <roy-lau@foxmail.com>
2017-06-16 18:00:14 +0800

    :clock1::clock3::zap:定时任务,每天自动上传项目:zap::clock5::clock6:---2017/06/16-18:00:03

--- koa/hyhWO/routes/index.js
+++ koa/hyhWO/routes/index.js
@@ -8,7 +8,50 @@ router.get('/', function *(next) {
 
 router.get('/product/Corporate-nickname', function *(next) {
   yield this.render('/pages/product/voiceClass/Corporate-nickname', {
-    title: '号外号-产品'
+    title: '号外号-产品-企业外号'
+  });
+});
+router.get('/product/Security-privacy-number', function *(next) {
+  yield this.render('/pages/product/voiceClass/Security-privacy-number', {
+    title: '号外号-产品-安全隐私号'
+  });
+});
+router.get('/product/One-button-double-call', function *(next) {
+  yield this.render('/pages/product/voiceClass/One-button-double-call', {
+    title: '号外号-产品-一键双呼'
+  });
+});
+router.get('/product/Business-card', function *(next) {
+  yield this.render('/pages/product/voiceClass/Business-card', {
+    title: '号外号-产品-企业名片'
+  });
+});
+
+
+
+
+
+短信通知
+
+闪信通知
+router.get('/product/Corporate-nickname', function *(next) {
+  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
+    title: '号外号-产品-短信验证码'
+  });
+});
+router.get('/product/Corporate-nickname', function *(next) {
+  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
+    title: '号外号-产品-短信发送平台'
+  });
+});
+router.get('/product/Corporate-nickname', function *(next) {
+  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
+    title: '号外号-产品-企业外号'
+  });
+});
+router.get('/product/Corporate-nickname', function *(next) {
+  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
+    title: '号外号-产品-企业外号'
   });
 });
 
