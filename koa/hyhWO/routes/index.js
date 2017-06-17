87f117fe44ee9e9889bbe4ce0dce2636cd0f8a2a
roy-lau <roy-lau@foxmail.com>
2017-06-17 18:00:04 +0800

    :clock1::clock3::zap:定时任务,每天自动上传项目:zap::clock5::clock6:---2017/06/17-18:00:02

--- koa/hyhWO/routes/index.js
+++ koa/hyhWO/routes/index.js
@@ -6,6 +6,24 @@ router.get('/', function *(next) {
   });
 });
 
+router.get('/price', function *(next) {
+  yield this.render('/pages/price', {
+    title: '号外号-价格'
+  });
+});
+router.get('/experience', function *(next) {
+  yield this.render('/pages/experience', {
+    title: '号外号-体验'
+  });
+});
+router.get('/api-doc', function *(next) {
+  yield this.render('/pages/APIdoc', {
+    title: '号外号-开发文档'
+  });
+});
+
+
+
 router.get('/product/Corporate-nickname', function *(next) {
   yield this.render('/pages/product/voiceClass/Corporate-nickname', {
     title: '号外号-产品-企业外号'
@@ -28,30 +46,24 @@ router.get('/product/Business-card', function *(next) {
 });
 
 
-
-
-
-短信通知
-
-闪信通知
-router.get('/product/Corporate-nickname', function *(next) {
-  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
+router.get('/product/SMS-authentication-code', function *(next) {
+  yield this.render('/pages/product/smsClass/SMS-authentication-code', {
     title: '号外号-产品-短信验证码'
   });
 });
-router.get('/product/Corporate-nickname', function *(next) {
-  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
+router.get('/product/SMS-platform', function *(next) {
+  yield this.render('/pages/product/smsClass/SMS-platform', {
     title: '号外号-产品-短信发送平台'
   });
 });
-router.get('/product/Corporate-nickname', function *(next) {
-  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
-    title: '号外号-产品-企业外号'
+router.get('/product/SMS-notification', function *(next) {
+  yield this.render('/pages/product/smsClass/SMS-notification', {
+    title: '号外号-产品-短信通知'
   });
 });
-router.get('/product/Corporate-nickname', function *(next) {
-  yield this.render('/pages/product/voiceClass/Corporate-nickname', {
-    title: '号外号-产品-企业外号'
+router.get('/product/Notice-of-flashing-letters', function *(next) {
+  yield this.render('/pages/product/smsClass/Notice-of-flashing-letters', {
+    title: '号外号-产品-闪信通知'
   });
 });
 
