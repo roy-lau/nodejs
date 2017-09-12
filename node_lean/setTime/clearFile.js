0426969124c836be8882f079fa8f1d69a095d94e
roy-lau <roy-lau@foxmail.com>
2017-09-12 10:49:40 +0800

    time-clear-file

--- node_lean/setTime/clearFile.js
+++ node_lean/setTime/clearFile.js
@@ -1,5 +1,5 @@
 var schedule = require('node-schedule'),   // 导入定时任务的模块
-	exec = require('child_process').exec,child,     // 导入子进程的模块
+	exec = require('child_process').exec,     // 导入子进程的模块
 	path = require('path');
 
 var delPath = path.resolve(__dirname, '../downfile') // 要删除的路径 “__dirname:当前路径”
@@ -8,10 +8,10 @@ var delPath = path.resolve(__dirname, '../downfile') // 要删除的路径 “__
 var j = schedule.scheduleJob('00 00 24 * * 7', function(){
 	console.log("开始执行删除任务"+ new Date() )
 
-	child = exec('rm -rf '+  delPath ,function(err,out) {
+	var child = exec('rm -rf '+  delPath ,function(err,out) {
 	  console.log(out); err && console.log(err);
 	});
 
 	console.log("结束执行删除任务"+ new Date() )
+	j.cancel()
 });
-j.cancel()
