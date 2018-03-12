6fd9c5e8cb763d168f45c0e45d8302765da4dc5c
roy-lau <roy-lau@foxmail.com>
2018-03-12 16:25:23 +0800

    bbs-koa: init

---
+++ koa/bbs-koa/src/index.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+// 错误处理
+app.on('error', (err, ctx) => {
+  log.error('server error', err, ctx)
+});
+
+app.listen(3000);
