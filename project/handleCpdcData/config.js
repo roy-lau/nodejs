df1eb810d376cc51ea7478283e924cc36d5a8d72
roy-lau <roy-lau@foxmail.com>
2019-10-14 16:18:00 +0800

    handleCpdc-handleFollw: 根据长海给的数据生成SQL

--- project/handleCpdcData/config.js
+++ project/handleCpdcData/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	db_addr: 'mssql://sa:sa@123@192.168.1.253/RYCPDC_C20190902',
+	db_addr: 'mssql://sa:sa@123@192.168.1.253/RYCPDC_C20191014',
+	db_addr1: 'mssql://sa:sa@123@192.168.1.253/RYCPDC_C20190902',
 	access_addr:'Provider=Microsoft.Jet.OleDb.4.0;Data Source=RYCPDC.mdb;Jet OLEDB:Database Password=sa@123',
 	access_addr_c:'Provider=Microsoft.Jet.OleDb.4.0;Data Source=C:\\SmartMedical\\CPDC\\Database\\RYCPDC.mdb;Jet OLEDB:Database Password=sa@123',
 	access_addr_loaclhost:'Provider=Microsoft.Jet.OleDb.4.0;Data Source=localhost\\Database\\RYCPDC.mdb;Jet OLEDB:Database Password=sa@123',
