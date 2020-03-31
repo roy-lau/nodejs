8dea0e00f2227c1db82d43552dbabfaa87630b31
roy-lau <roylau_vip@163.com>
2020-03-31 13:56:51 +0800

    cpdc-g: 初步搞定对文件的加密

--- project/cpdc-g/g/utils/rea-file.js
+++ project/cpdc-g/g/utils/rea-file.js
@@ -1,125 +1,54 @@
-let fs = require('fs');
-let zlib = require('zlib');
-let crypto = require('crypto');
+/**
+ * 文件加密解密
+ * 参考： http://nodejs.cn/api/crypto.html
+ */
+const crypto = require('crypto');
+const zlib = require('zlib');
+const fs = require('fs');
 
+const algorithm = 'aes-192-cbc';
+const password = '用于生成密钥的密码';
+// 改为使用异步的 `crypto.scrypt()`。
+const key = crypto.scryptSync(password, '盐值', 24);
+// 使用 `crypto.randomBytes()` 生成随机的 iv 而不是此处显示的静态的 iv。
+const iv = Buffer.alloc(16, 0); // 初始化向量。
 
-function geFileList (path) {
-    let filesList = [];
-    readFile(path, filesList);
-    return filesList;
+/**
+ * 解密文件
+ * @param {String} fileName 文件名
+ */
+function encryptFile (fileName) {
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    const input = fs.createReadStream(fileName);
+    const output = fs.createWriteStream(fileName + '.enc');
+
+    input
+        // .pipe(zlib.createGzip())
+        .pipe(cipher)
+        .pipe(output);
+    console.log(fileName + ' 加密成功')
 }
 
-function readFile (path, filesList) {
-    files = fs.readdirSync(path);
-    files.forEach(walk);
-    function walk (file) {
-        states = fs.statSync(path + '/' + file);
-        if (states.isDirectory()) {
-            readFile(path + '/' + file, filesList);
-        }
-        else {
-            let obj = new Object();
-            obj.size = states.size;
-            obj.name = file;
-            obj.path = path + '/' + file;
-            filesList.push(obj);
-        }
-    }
+/**
+ * 加密文件
+ * @param {String} fileName 文件名
+ */
+function decryptFile (fileName) {
+
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+    const input = fs.createReadStream(fileName + '.enc');
+    const output = fs.createWriteStream(fileName);
+
+    input
+        // .pipe(zlib.createGunzip())
+        .pipe(decipher)
+        .pipe(output);
+    console.log(fileName + ' 解密成功')
 }
 
-let getFileName = function (path) {
-    let pathList = path.split("/");
-    let fileName = pathList[pathList.length - 1];
-    return fileName;
-};
+const fileName = 'demo.txt'
 
-
-let getFileContent = function (filePath, cb) {
-    fs.readFile(filePath, function (err, buf) {
-        cb(err, buf);
-    });
-};
-
-let writeFileSync = function (filePath, text) {
-    fs.writeFileSync(filePath, text);
-};
-
-let writeFileAsync = function (filePath, text, cb) {
-    fs.writeFile(filePath, text, function (err) {
-        cb(err);
-    });
-};
-
-/********************加密解密部分**************************/
-
-let consts = {
-    cryptkey: 'S62rgt9rf!nYS5b3',
-    iv: "Og'Y6Jm-'i#io9Op"
-};
-
-function util () {
-
-}
-let prot = util.prototype;
-
-prot.md5 = function (str) {
-    let md5sum = crypto.createHash('md5');
-    md5sum.update(str);
-    str = md5sum.digest('hex');
-    return str;
-};
-
-prot.encode = function (content) {
-    let cipher = crypto.createCipheriv('aes-128-cbc', consts.cryptkey, consts.iv);
-    cipher.setAutoPadding(true);
-    let bf = [];
-    bf.push(cipher.update(content));
-    bf.push(cipher.final());
-    return Buffer.concat(bf);
-};
-
-
-prot.decode = function (content) {
-    let decipher = crypto.createDecipheriv('aes-128-cbc', consts.cryptkey, consts.iv);
-    decipher.setAutoPadding(true);
-    try {
-        let a = [];
-        a.push(decipher.update(content));
-        a.push(decipher.final());
-        return Buffer.concat(a);
-    } catch (e) {
-        console.error('decode error:', e.message);
-        return null;
-    }
-};
-
-
-/**********************压缩解压缩部分************************/
-function gZip (strText, cb) {
-    zlib.gzip(strText, function (err, bufData) {
-        cb(err, bufData);
-    });
-}
-
-function unZip (buffer, cb) {
-    zlib.unzip(buffer, function (err, buf) {
-        cb(err, buf);
-    });
-}
-
-
-
-module.exports = {
-    "FileUtil": {
-        "getDirFiles": geFileList,
-        "getFileName": getFileName,
-        "writeFileAsync": writeFileAsync,
-        "writeFileSync": writeFileSync,
-        "getFileContent": getFileContent
-    },
-    "EncodeUtil": util,
-    "ZipUtil": {
-        "gZip": gZip,
-        "unZip": unZip
-    }
-};
+// encryptFile(fileName)
+// decryptFile(fileName)
