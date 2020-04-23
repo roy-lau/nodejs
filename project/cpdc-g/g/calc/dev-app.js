5a8a2ed0da2c8e1e7b63b956cb080790baee3c20
roy-lau <roylau_vip@163.com>
2020-04-23 09:30:05 +0800

    cpdc-g: clac 生成的表格进行压缩(64k to 16k),尝试分医院统计

--- project/cpdc-g/g/calc/dev-app.js
+++ project/cpdc-g/g/calc/dev-app.js
@@ -6,6 +6,7 @@
 'use strict';
 const sql = require('../dbs/sqlServer-t.js'),
     XLSX = require("xlsx"),
+    X_utils = XLSX.utils,
     _ = require("lodash"),
     // fs = require("fs-extra"),
     ProgressBar = require('progress'),
@@ -15,55 +16,53 @@ const sql = require('../dbs/sqlServer-t.js'),
 // 保存计算结果
 async function saveCalcResult (fileName) {
     try {
-        let data = [], _num = 0
-
-        delete OBJ_CALC._ids
-
-        for (const key in OBJ_CALC) {
-            const value = OBJ_CALC[key];
-            // console.log(key)
-            // const itemCode = key.split('#')[1]
-            // if (itemCode) {
-            //     const ret = await sql.query(`SELECT
-            //         isnull( title.ITEM_TYPE_NAME, '' ) + '_' + isnull( title1.ITEM_TYPE_NAME, '' ) AS 'typeName'
-            //     FROM
-            //         [dbo].[SD_ITEM_TYPE_DICT] AS title 
-            //         LEFT JOIN [dbo].[SD_ITEM_TYPE_DICT] AS title1 ON title.ITEM_TYPE_CODE=title1.PARENT_TYPE_CODE AND title1.PARENT_TYPE_CODE IS NOT NULL AND title.SD_CODE='YXA_O' 
-            //         LEFT JOIN [dbo].[SD_ITEM_DICT] AS dist ON dist.ITEM_TYPE_CODE=title.ITEM_TYPE_CODE OR dist.ITEM_TYPE_CODE=title1.ITEM_TYPE_CODE AND dist.SD_CODE='YXA_O'
-            //         LEFT JOIN [dbo].[FU_SD_ITEM_DICT] AS fu_dist ON fu_dist.ITEM_TYPE_CODE=title.ITEM_TYPE_CODE
-            //     WHERE
-            //         title.PARENT_TYPE_CODE IS NULL 
-            //         AND title.SD_CODE= 'YXA_O'
-            //         AND dist.ITEM_CODE='${itemCode}'`)
-            //     const itemType = ret[0]
-            //     if (itemType) {
-            //         data.push({ '类别': itemType.typeName, '数据项': key, '完整率': value })
-            //     } else {
-            //     }
-            // }
-            data.push({ '数据项': key, '完整率': value })
-        }
-        // TODO： 计算有误，后期处理
-        // _num += Number(value.replace("%", ""))
-        // const totalPercent = ((data.length * 10000) / _num ).toFixed(2) + '%'
-        // data.push({ '数据项': '总完整率', '完整率': totalPercent })
-
-        // 构建 workbook 对象
-        let wb = {
-            SheetNames: ['统计字段完整率'],
-            Sheets: {
-                '统计字段完整率': XLSX.utils.json_to_sheet(data),
-            }
-        }
-
-        // 导出 Excel
-        XLSX.writeFile(wb, './out/字段完整度_' + fileName + '.xlsx');
-        console.log(fileName, '-OK ', Date.now())
+        const data = await getCalcData()
+        // 创建表格
+        const wb = X_utils.book_new()
+        // 插入 sheet
+        X_utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(data), "统计字段完整率")
+        // 保存 Excel 文件
+        XLSX.writeFile(wb, './out/字段完整度_' + fileName + Date.now() + '.xlsx');
+        OBJ_CALC = Object.create(null) // 清空对象
+        console.log('文件：',fileName,' 保存成功！')
     } catch (e) {
-        console.error('处理表格数据出错： ', e)
+        console.error('保存表格出错： ', e)
     }
 }
 
+async function getCalcData () {
+    const allDict = await sql.query(`SELECT
+            isnull( title.ITEM_TYPE_NAME, '' ) + isnull( '_' + title1.ITEM_TYPE_NAME, '' )  AS 'typeName',
+            isnull( dist.ITEM_CODE, fu_dist.ITEM_CODE ) AS 'itemCode'
+        FROM
+            [dbo].[SD_ITEM_TYPE_DICT] AS title 
+            LEFT JOIN [dbo].[SD_ITEM_TYPE_DICT] AS title1 ON title.ITEM_TYPE_CODE=title1.PARENT_TYPE_CODE AND title1.PARENT_TYPE_CODE IS NOT NULL AND title.SD_CODE='YXA_O' 
+            LEFT JOIN [dbo].[SD_ITEM_DICT] AS dist ON dist.ITEM_TYPE_CODE=title.ITEM_TYPE_CODE OR dist.ITEM_TYPE_CODE=title1.ITEM_TYPE_CODE AND dist.SD_CODE='YXA_O'
+            LEFT JOIN [dbo].[FU_SD_ITEM_DICT] AS fu_dist ON fu_dist.ITEM_TYPE_CODE=title.ITEM_TYPE_CODE
+        WHERE
+            title.PARENT_TYPE_CODE IS NULL 
+            AND title.SD_CODE= 'YXA_O'`)
+    let data = [], _num = 0
+    delete OBJ_CALC._ids
+    for (const key in OBJ_CALC) {
+        const value = OBJ_CALC[key];
+        const itemCode = key.split('#')[1]
+        if (itemCode) {
+            const [cutItem] = allDict.filter(dict => dict.itemCode == itemCode)
+            if (cutItem) {
+                data.push({ '类别': cutItem.typeName, '数据项': key, '完整率': value + '%' })
+            }
+        } else {
+            const [typeName, itemName] = key.split('_')
+            data.push({ '类别': typeName, '数据项': itemName, '完整率': value + '%' })
+        }
+        _num += Number(value)
+    }
+    // push 总完整率
+    const totalPercent = (_num / data.length).toFixed(2) + '%'
+    data.push({ '数据项': '总完整率', '完整率': totalPercent })
+    return data
+}
 /**
  * 计算 随访结果表 ---  验证通过
  * 
@@ -97,7 +96,7 @@ async function handlePatFollowUpResult (index, patNo) {
             if (_items.SD_ITEM_VALUE || needExits(retPatFollowUpResult, _items)) {
                 followDist[joinKey + '_num'] += 1
             }
-            OBJ_CALC[joinKey] = (followDist[joinKey + '_num'] / followDist[joinKey + '_total'] * 100).toFixed(2) + '%'
+            OBJ_CALC[joinKey] = (followDist[joinKey + '_num'] / followDist[joinKey + '_total'] * 100).toFixed(2)
         }
     } catch (err) {
         console.error('handlePatFollowUpResult ERR ', err)
@@ -139,22 +138,22 @@ const initFollowTreat = Object.create(null)
 async function handlePatFollowUpTreat (index, patNo) {
     try {
         const retPatFollowUpTreat = await sql.query(`SELECT
-            TREAT_NAME AS '治疗方式',
-            DRUG_NAME AS '药品名称(通用名)',
-            DRUG_NAME_TRADE AS '药品名称(商品名)',
-            DRUG_DOSE AS '剂量',
-            TREAT_CYCLE AS '疗程/周期',
-            TREAT_METHOD AS '化疗方法',
-            TREAT_EFFECT AS '是否好转',
-            TREAT_COST AS '化疗费用',
-            CA199_FRONT AS '治疗前CA199',
-            CEA_FRONT AS '治疗前CEA',
-            CA125_FRONT AS '治疗前CA125',
-            TREAT_EVALUTE_FRONT AS '治疗前CT评价',
-            CA199_AFTER AS '治疗后CA199',
-            CEA_AFTER AS '治疗后CEA',
-            CA125_AFTER AS '治疗后CA125',
-            TREAT_EVALUTE_AFTER AS '治疗后CT评价'
+            TREAT_NAME AS '随访_治疗方式',
+            DRUG_NAME AS '随访_药品名称(通用名)',
+            DRUG_NAME_TRADE AS '随访_药品名称(商品名)',
+            DRUG_DOSE AS '随访_剂量',
+            TREAT_CYCLE AS '随访_疗程/周期',
+            TREAT_METHOD AS '随访_化疗方法',
+            TREAT_EFFECT AS '随访_是否好转',
+            TREAT_COST AS '随访_化疗费用',
+            CA199_FRONT AS '随访_治疗前CA199',
+            CEA_FRONT AS '随访_治疗前CEA',
+            CA125_FRONT AS '随访_治疗前CA125',
+            TREAT_EVALUTE_FRONT AS '随访_治疗前CT评价',
+            CA199_AFTER AS '随访_治疗后CA199',
+            CEA_AFTER AS '随访_治疗后CEA',
+            CA125_AFTER AS '随访_治疗后CA125',
+            TREAT_EVALUTE_AFTER AS '随访_治疗后CT评价'
 		FROM
 			[dbo].[PAT_FOLLOW_UP_TREAT]
 		WHERE
@@ -163,7 +162,22 @@ async function handlePatFollowUpTreat (index, patNo) {
 
         // console.log(key,index,_followUpTreatTotal)
         if (index == 0) { // 第一次，初始化（如果第一个为空，将会报错 query2）
-            const _keys = ['治疗方式', '药品名称(通用名)', '药品名称(商品名)', '剂量', '疗程/周期', '化疗方法', '是否好转', '化疗费用', '治疗前CA199', '治疗前CEA', '治疗前CA125', '治疗前CT评价', '治疗后CA199', '治疗后CEA', '治疗后CA125', '治疗后CT评价']
+            const _keys = ['随访_治疗方式',
+                '随访_药品名称(通用名)',
+                '随访_药品名称(商品名)',
+                '随访_剂量',
+                '随访_疗程/周期',
+                '随访_化疗方法',
+                '随访_是否好转',
+                '随访_化疗费用',
+                '随访_治疗前CA199',
+                '随访_治疗前CEA',
+                '随访_治疗前CA125',
+                '随访_治疗前CT评价',
+                '随访_治疗后CA199',
+                '随访_治疗后CEA',
+                '随访_治疗后CA125',
+                '随访_治疗后CT评价']
             for (let k = 0; k < _keys.length; k++) {
 
                 initFollowTreat[_keys[k] + '_total'] = 0
@@ -179,7 +193,7 @@ async function handlePatFollowUpTreat (index, patNo) {
                 if (value) {
                     initFollowTreat[key + '_num'] += 1
                 }
-                OBJ_CALC[key] = (initFollowTreat[key + '_num'] / initFollowTreat[key + '_total'] * 100).toFixed(2) + '%'
+                OBJ_CALC[key] = (initFollowTreat[key + '_num'] / initFollowTreat[key + '_total'] * 100).toFixed(2)
             });
         }
     } catch (err) {
@@ -222,8 +236,8 @@ async function handlePatFollowUp (index, patNo) {
         /**
          * 待优化，（参与计算次数过多，可以只在最后一次进行计算。但是写法不够优雅）
          */
-        OBJ_CALC['随访时间'] = (followDate_num / follow_total * 100).toFixed(2) + '%'
-        OBJ_CALC['随访时长'] = (followMonths_num / follow_total * 100).toFixed(2) + '%'
+        OBJ_CALC['随访_随访时间'] = (followDate_num / follow_total * 100).toFixed(2)
+        OBJ_CALC['随访_随访时长'] = (followMonths_num / follow_total * 100).toFixed(2)
     } catch (err) {
         console.error('handlePatFollowUp ERR ', err)
     }
@@ -238,18 +252,17 @@ async function handlePatFollowUp (index, patNo) {
 const initDrainageTube = Object.create(null)
 async function handlePatDrainageTube (index, patNo) {
     try {
-
         const retPatDrainageTube = await sql.query(`SELECT
                 -- PATIENT_NO,
-                TUBE_NAME AS '引流管部位',
-                RETENTION_DAYS AS '留置天数',
-                POD1,
-                POD3,
-                POD7,
-                AMY_POD1,
-                AMY_POD3,
-                AMY_POD7,
-                AMY_POD_DRAW AS '拔管前AMY'
+                TUBE_NAME AS '引流管_引流管部位',
+                RETENTION_DAYS AS '引流管_留置天数',
+                POD1 AS '引流管_POD1',
+                POD3 AS '引流管_POD3',
+                POD7 AS '引流管_POD7',
+                AMY_POD1 AS '引流管_AMYPOD1',
+                AMY_POD3 AS '引流管_AMYPOD3',
+                AMY_POD7 AS '引流管_AMYPOD7',
+                AMY_POD_DRAW AS '引流管_拔管前AMY'
             FROM
                 [dbo].[PAT_DRAINAGE_TUBE]
             WHERE
@@ -258,7 +271,7 @@ async function handlePatDrainageTube (index, patNo) {
 
         // console.log(key,index,_followUpTreatTotal)
         if (index == 0) { // 第一次，初始化（如果第一个为空，将会报错 query）
-            const _keys = ['引流管部位', '留置天数', 'POD1', 'POD3', 'POD7', 'AMY_POD1', 'AMY_POD3', 'AMY_POD7', '拔管前AMY']
+            const _keys = ['引流管_引流管部位', '引流管_留置天数', '引流管_POD1', '引流管_POD3', '引流管_POD7', '引流管_AMYPOD1', '引流管_AMYPOD3', '引流管_AMYPOD7', '引流管_拔管前AMY']
             for (let k = 0; k < _keys.length; k++) {
 
                 initDrainageTube[_keys[k] + '_total'] = 0
@@ -280,14 +293,13 @@ async function handlePatDrainageTube (index, patNo) {
                     initDrainageTube[key + '_total'] += 1
                     initDrainageTube[key + '_num'] += 0
                 }
-                OBJ_CALC[key] = (initDrainageTube[key + '_num'] / initDrainageTube[key + '_total'] * 100).toFixed(2) + '%'
+                OBJ_CALC[key] = (initDrainageTube[key + '_num'] / initDrainageTube[key + '_total'] * 100).toFixed(2)
             });
         }
     } catch (err) {
         console.error('handlePatDrainageTube ERR ', err)
     }
 }
-
 /**
  * 计算 数据项结果 --- 验证通过
  * @param {Number} index 
@@ -314,7 +326,7 @@ async function handlePatItemResult (index, patNo) {
                 WHERE
                     dict.SD_CODE = 'YXA_O'
                 ORDER BY
-                    DISPLAY_ORDER+0`),
+                    dict.DISPLAY_ORDER+0`),
             _lenPatItemResult = retPatItemResult.length
 
         // console.log(key,index,retPatItemResult)
@@ -328,7 +340,7 @@ async function handlePatItemResult (index, patNo) {
             if (_items.SD_ITEM_VALUE || needExits(retPatItemResult, _items)) {
                 initPatItemResult[joinKey + '_num'] += 1
             }
-            OBJ_CALC[joinKey] = (initPatItemResult[joinKey + '_num'] / initPatItemResult[joinKey + '_total'] * 100).toFixed(2) + '%'
+            OBJ_CALC[joinKey] = (initPatItemResult[joinKey + '_num'] / initPatItemResult[joinKey + '_total'] * 100).toFixed(2)
         }
 
     } catch (err) {
@@ -339,7 +351,8 @@ async function handlePatItemResult (index, patNo) {
  * 判断是否需要计入完整度(诊治结果表 和 随访结果表都调用里这个函数)
  * @param  {[type]} data  [description]
  * @param  {[type]} items [description]
- * @return {[type]}        true OR null
+ * @return {[type]}        true 是，不参与计算 。 null 否，参与计算
+ * @description 父项为 是 和 空 的参与计算，为否的不参与计算
  */
 function needExits (data, items) {
     // console.log(items)
@@ -390,14 +403,14 @@ async function handlePatVist (index, patNo) {
     try {
         const retPatVist = await sql.query(`SELECT
                         PATIENT_NO,
-                        PATIENT_ID AS '住院ID',
-                        INP_NO AS '住院流水号',
-                        NAME AS '患者姓名',
-                        SEX AS '性别',
-                        AGE AS '患者年龄',
-                        ADMISSION_DATE AS '入院日期',
-                        DISCHARGE_DATE AS '出院日期',
-                        OUT_STATUS AS '离院方式'
+                        PATIENT_ID AS '基本信息_住院ID',
+                        INP_NO AS '基本信息_住院流水号',
+                        NAME AS '基本信息_患者姓名',
+                        SEX AS '基本信息_性别',
+                        AGE AS '基本信息_患者年龄',
+                        ADMISSION_DATE AS '基本信息_入院日期',
+                        DISCHARGE_DATE AS '基本信息_出院日期',
+                        OUT_STATUS AS '基本信息_离院方式'
                     FROM
                         [dbo].[PAT_VISIT]
                     WHERE
@@ -419,7 +432,7 @@ async function handlePatVist (index, patNo) {
             if (value) {
                 initPatVist[key + '_num'] += 1
             }
-            OBJ_CALC[key] = (initPatVist[key + '_num'] / initPatVist[key + '_total'] * 100).toFixed(2) + '%'
+            OBJ_CALC[key] = (initPatVist[key + '_num'] / initPatVist[key + '_total'] * 100).toFixed(2)
         });
     } catch (err) {
         console.error('handlePatVist ERR ', err)
@@ -430,36 +443,48 @@ function startup () {
 
     console.time("共用时")
 
-    const querySql = require("./querySql.js")
+    sql.query("SELECT HOSPITAL_ID,HOSPITAL_NAME FROM [dbo].[HOSPITAL_DICT]").then(async hList => {
+        for (let i = 0; i <= hList.length; i++) {
+            const h = hList[i]
+            console.log(h.HOSPITAL_NAME)
+            const retPatientNo = await sql.query(`SELECT PATIENT_NO FROM [dbo].[PAT_VISIT] WHERE SD_CODE='YXA_O' AND SD_GROUP='1' AND HOSPITAL_ID='${h.HOSPITAL_ID}'`)
+            console.log(retPatientNo)
+            const len = retPatientNo.length
+            if (len < 1) return
+            let bar = new ProgressBar('  进度 [:bar] :current/:total :percent :etas', {
+                complete: '=', // 完成
+                incomplete: ' ', // 未完成
+                width: 50, // 宽度
+                total: len // 总数
+            });
 
-    sql.query(querySql).then(async retPatientNo => {
-        const len = retPatientNo.length
-        if (len < 1) throw "sql.query 查询结果小于 0"
-        let bar = new ProgressBar('  进度 [:bar] :current/:total :percent :etas', {
-            complete: '=', // 完成
-            incomplete: ' ', // 未完成
-            width: 50, // 宽度
-            total: len // 总数
-        });
+            for (let index = 0; index < len; index++) {
+                const element = retPatientNo[index],
+                    patNo = element.PATIENT_NO;
 
-        for (let index = 0; index < len; index++) {
-            const element = retPatientNo[index],
-                patNo = element.PATIENT_NO;
+                await handlePatVist(index, patNo)
+                await handlePatItemResult(index, patNo)
+                await handlePatDrainageTube(index, patNo)
 
-            await handlePatVist(index, patNo)
-            await handlePatItemResult(index, patNo)
-            await handlePatDrainageTube(index, patNo)
+                await handlePatFollowUp(index, patNo)
 
-            await handlePatFollowUp(index, patNo)
+                await handlePatFollowUpTreat(index, patNo)
+                await handlePatFollowUpResult(index, patNo)
+                bar.tick();
 
-            await handlePatFollowUpTreat(index, patNo)
-            await handlePatFollowUpResult(index, patNo)
-            bar.tick();
-
+            }
+            let data = [], _num = 0
+            delete OBJ_CALC._ids
+            for (const key in OBJ_CALC) {
+                const value = OBJ_CALC[key];
+                _num += Number(value)
+            }
+            // push 总完整率
+            const totalPercent = (_num / data.length).toFixed(2) + '%'
+            data.push({ '数据项': '总完整率', '完整率': totalPercent })
+            // await saveCalcResult(h.HOSPITAL_NAME)
+            console.timeEnd("共用时")
         }
-
-        await saveCalcResult('金巍巍')
-        console.timeEnd("共用时")
     })
 }
 startup()
--- project/cpdc-g/g/calc/querySql.js
+++ project/cpdc-g/g/calc/querySql.js
@@ -86,6 +86,27 @@ const jinWeiWei = `SELECT
         AND PATIENT_NO IN ( SELECT DISTINCT PATIENT_NO FROM [dbo].[PAT_SD_ITEM_RESULT] WHERE SD_ITEM_CODE = 'YXA_O_224' AND SD_ITEM_VALUE = '2' ) 
     )`
 
+// 金巍巍2
+const jinWeiWei2 = `SELECT DISTINCT PATIENT_NO  FROM [dbo].[PAT_SD_ITEM_RESULT] WHERE SD_ITEM_CODE='YXA_O_210' AND SD_ITEM_VALUE LIKE '%导管%腺癌%' AND PATIENT_NO IN (
+    SELECT 
+        DISTINCT PATIENT_NO 
+        FROM
+        [dbo].[PAT_SD_ITEM_RESULT] 
+        WHERE
+        SD_ITEM_CODE = 'YXA_O_152'
+        AND SD_ITEM_VALUE IN ( '1', '3' ) 
+        AND PATIENT_NO IN (
+        SELECT DISTINCT
+            PATIENT_NO 
+        FROM
+            [dbo].[PAT_SD_ITEM_RESULT] 
+        WHERE
+            SD_ITEM_CODE = 'YXA_O_151' 
+            AND SD_ITEM_VALUE IN ( '1', '2', '3' ) 
+            AND PATIENT_NO IN ( SELECT DISTINCT PATIENT_NO FROM [dbo].[PAT_SD_ITEM_RESULT] WHERE SD_ITEM_CODE = 'YXA_O_224' AND SD_ITEM_VALUE = '2' ) 
+        )
+    
+    )`
 // 一期医院
 const firstPhaseOfHospital= `SELECT
     PATIENT_NO 
@@ -104,8 +125,10 @@ const groupInFolluw = `SELECT
     FOLLOW_UP_DATE != '1900-01-01 00:00:00.000'
     AND FOLLOW_UP_MONTHS!=''
     AND PATIENT_NO IN ( SELECT PATIENT_NO FROM [dbo].[PAT_VISIT] WHERE SD_GROUP = '1' AND SD_CODE = 'YXA_O' )`
-
+// 广东省人民
 const guangdong = `SELECT PATIENT_NO FROM [dbo].[PAT_VISIT] WHERE HOSPITAL_ID='54bc034b46af0a8d' AND SD_CODE='YXA_O'`
+
+const one = `SELECT PATIENT_NO FROM [dbo].[PAT_VISIT] WHERE PATIENT_NO IN ('42c4ff6470cac2ba','86bb1a938fc08710','5b4f0f0181fac940') AND SD_CODE='YXA_O'`
 // 三年内入组的患者
 const threeYearInGroup = `SELECT
     TOP 5000
@@ -118,4 +141,4 @@ const threeYearInGroup = `SELECT
     AND DISCHARGE_DATE >= '2016-01-01 00:00:00.000'
     AND DISCHARGE_DATE <= '2018-12-31 00:00:00.000'`
 
-module.exports = guangdong
+module.exports = one
