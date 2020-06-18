12cd5e78b82281af518f6542d9388601486a7fca
roylau <roy-lau@foxmail.com>
2020-06-18 16:27:29 +0800

    cpdc-g: 增加新的入组SQL，增加根据患者所在医院的地址查询省市大区sql

--- project/cpdc-g/g/docs/sql/入组SQL-new.sql
+++ project/cpdc-g/g/docs/sql/入组SQL-new.sql
@@ -1,4 +1,3 @@
-
 UPDATE PAT_VISIT
 SET SD_GROUP = 1
 WHERE
@@ -16,14 +15,31 @@ WHERE
     AND b.SD_ITEM_CODE= 'YXA_O_210'
     AND a.SD_CODE= 'YXA_O'
     AND (
-      ( b.SD_ITEM_VALUE LIKE '%癌%' OR b.SD_ITEM_VALUE LIKE '%恶%' )
+      ( b.SD_ITEM_VALUE LIKE '%[^样]癌%' OR b.SD_ITEM_VALUE LIKE '%恶%' )
       AND (
-        b.SD_ITEM_VALUE NOT LIKE '%十二指肠%'
+        b.SD_ITEM_VALUE NOT LIKE '%十二指%'
         AND b.SD_ITEM_VALUE NOT LIKE '%壶腹%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%透明%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%间变%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%印戒%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%[腺滤]泡%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%未分化%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%肉瘤%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%小细胞%'
         AND b.SD_ITEM_VALUE NOT LIKE '%内分泌%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%导管癌%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%假乳头%'
         AND b.SD_ITEM_VALUE NOT LIKE '%未见%'
-        AND b.SD_ITEM_VALUE NOT LIKE '%胆管%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%胆[管囊]%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%肠型%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%鳞[状形]%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%[黏粘浆]液%'
         AND b.SD_ITEM_VALUE NOT LIKE '%胆总管%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%胃%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%甲状腺%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%肾%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%[肝髓胶骨]样%'
+        AND b.SD_ITEM_VALUE NOT LIKE '%淋巴瘤%'
       )
     ) UNION
   SELECT
@@ -38,7 +54,7 @@ WHERE
     AND a.SD_CODE= b.SD_CODE
     AND b.SD_ITEM_CODE= 'YXA_O_210'
     AND a.SD_CODE= 'YXA_O'
-    AND ( b.SD_ITEM_VALUE LIKE '%癌%' OR b.SD_ITEM_VALUE LIKE '%恶%' )
+    AND ( b.SD_ITEM_VALUE LIKE '%[^管]癌%' OR b.SD_ITEM_VALUE LIKE '%恶%' )
     AND ( b.SD_ITEM_VALUE LIKE '%胰%' OR b.SD_ITEM_VALUE LIKE '%导管%' )
     AND b.SD_ITEM_VALUE NOT LIKE '%胰%内分泌%癌%' UNION
   SELECT
--- project/cpdc-g/g/docs/sql/查询患者所在的省市.sql
+++ project/cpdc-g/g/docs/sql/查询患者所在的省市.sql
@@ -71,3 +71,41 @@ WHERE
 	AND r.SD_CODE='YXA_O'
 	GROUP BY area.AREA_NAME
 	ORDER BY num DESC
+
+
+-- ----------------------------
+-- 查询患者所在医院的 省市 大区
+-- ----------------------------
+
+-- 查询患者所在医院 的省市
+SELECT
+	u.PATIENT_NO,
+	h.HOSPITAL_NAME,
+	(CASE c.PARENT_ID WHEN '0' 
+		THEN c.CITY_NAME ELSE ( SELECT a.CITY_NAME FROM [dbo].[CITY] AS a WHERE a.PARENT_ID= '0' AND a.ID= c.PARENT_ID ) 
+		END ) AS 'province' 
+	c.CITY_NAME 
+FROM
+	[dbo].[PAT_VISIT] AS u
+	LEFT JOIN [dbo].[HOSPITAL_DICT] AS h ON u.HOSPITAL_ID= h.HOSPITAL_ID
+	LEFT JOIN [dbo].[CITY] AS c ON c.CITY_NAME= REPLACE( h.HOSPITAL_CITY, '市', '' )
+
+
+-- 查询患者所在医院 所在的的大区
+SELECT
+	area.AREA_NAME,
+	COUNT ( area.AREA_NAME ) 
+FROM
+	(SELECT
+			(CASE c.PARENT_ID WHEN '0' 
+				THEN c.CITY_NAME ELSE ( SELECT a.CITY_NAME FROM [dbo].[CITY] AS a WHERE a.PARENT_ID= '0' AND a.ID= c.PARENT_ID ) 
+				END ) AS 'province' 
+		FROM
+			[dbo].[PAT_VISIT] AS u
+			LEFT JOIN [dbo].[HOSPITAL_DICT] AS h ON u.HOSPITAL_ID= h.HOSPITAL_ID
+			LEFT JOIN [dbo].[CITY] AS c ON c.CITY_NAME= REPLACE( h.HOSPITAL_CITY, '市', '' ) 
+			-- WHERE u.PATIENT_NO IN () -- 留着用了选择查询
+		) AS list_c
+		LEFT JOIN [dbo].[AREA_CITY] AS area ON list_c.province= area.CITY_NAME 
+GROUP BY
+	area.AREA_NAME
