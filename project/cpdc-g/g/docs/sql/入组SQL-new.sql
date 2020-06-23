e2c079f2bc5f3dad8e60595b28513dd172ec38b0
roylau <roy-lau@foxmail.com>
2020-06-23 11:24:14 +0800

    cpdc-g: sql 更新入组SQL

--- project/cpdc-g/g/docs/sql/入组SQL-new.sql
+++ project/cpdc-g/g/docs/sql/入组SQL-new.sql
@@ -1,3 +1,4 @@
+UPDATE PAT_VISIT SET SD_GROUP =NULL
 UPDATE PAT_VISIT
 SET SD_GROUP = 1
 WHERE
@@ -15,7 +16,7 @@ WHERE
     AND b.SD_ITEM_CODE= 'YXA_O_210'
     AND a.SD_CODE= 'YXA_O'
     AND (
-      ( b.SD_ITEM_VALUE LIKE '%[^样]癌%' OR b.SD_ITEM_VALUE LIKE '%恶%' )
+        b.SD_ITEM_VALUE LIKE '%腺癌%'
       AND (
         b.SD_ITEM_VALUE NOT LIKE '%十二指%'
         AND b.SD_ITEM_VALUE NOT LIKE '%壶腹%'
@@ -54,9 +55,8 @@ WHERE
     AND a.SD_CODE= b.SD_CODE
     AND b.SD_ITEM_CODE= 'YXA_O_210'
     AND a.SD_CODE= 'YXA_O'
-    AND ( b.SD_ITEM_VALUE LIKE '%[^管]癌%' OR b.SD_ITEM_VALUE LIKE '%恶%' )
-    AND ( b.SD_ITEM_VALUE LIKE '%胰%' OR b.SD_ITEM_VALUE LIKE '%导管%' )
-    AND b.SD_ITEM_VALUE NOT LIKE '%胰%内分泌%癌%' UNION
+    AND b.SD_ITEM_VALUE LIKE '%腺癌%'
+    AND b.SD_ITEM_VALUE LIKE '%导管%' UNION
   SELECT
     a.PATIENT_NO --distinct b.SD_ITEM_VALUE
 --(select HOSPITAL_NAME from HOSPITAL_DICT where HOSPITAL_ID=a.HOSPITAL_ID) as hospital_name,a.HOSPITAL_ID,b.*
@@ -72,3 +72,26 @@ WHERE
     AND ( b.SD_ITEM_VALUE LIKE '%PDAC%' OR b.SD_ITEM_VALUE LIKE '%daoguanxianai%' )
   ) -- SELECT * FROM PAT_VISIT WHERE SD_CODE='yxa_o' and sd_group=1
 --update PAT_VISIT set SD_GROUP=null
+
+
+------------------------
+--  陈汝福
+------------------------
+
+
+UPDATE PAT_VISIT
+SET SD_GROUP = 1
+WHERE
+	PATIENT_NO IN (
+	SELECT
+		a.PATIENT_NO 
+	FROM
+		[PAT_VISIT] a,
+		PAT_SD_ITEM_RESULT b 
+	WHERE
+		a.PATIENT_NO= b.PATIENT_NO 
+		AND a.SD_CODE= b.SD_CODE 
+		AND b.SD_ITEM_CODE= 'YXA_O_210' 
+		AND a.SD_CODE= 'YXA_O' 
+	AND ( b.SD_ITEM_VALUE LIKE '%导管腺癌%' OR b.SD_ITEM_VALUE LIKE '%PDAC%' OR b.SD_ITEM_VALUE LIKE '%daoguanxianai%' ) 
+	)
